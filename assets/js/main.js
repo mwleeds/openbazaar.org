c4a4e4ad503bc9669409fa5dbf1945680910a267
James D. Wilson <james@jameswilson.name>
2015-10-03 10:14:22 -0500

    Incremental improvements - extracted inline css and js, formatted files, refactored js, removed redundant css, fixed path in css

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+$(function () {
+    var slideAdvanceDelay = 6000;
+    var animationDelay = 400;
+    var imagePath = 'assets/img/';
+
+    var currentSlide = 1;
+    var slides = [
+        {
+            sectionBackgroundColor: '#1F231D',
+            screenshot: 'screen-1.png',
+            overlayImage: 'background-1.jpg',
+            bodyAnimationBackgroundColor: '#2B3328'
+        },
+        {
+            sectionBackgroundColor: '#201520',
+            screenshot: 'screen-2.png',
+            overlayImage: 'background-2.jpg',
+            bodyAnimationBackgroundColor: '#201520'
+        },
+        {
+            sectionBackgroundColor: '#785A19',
+            screenshot: 'screen-3.png',
+            overlayImage: 'background-3.jpg',
+            bodyAnimationBackgroundColor: '#785A19'
+        }
+    ];
+
+    function advanceSlide() {
+        var slide = slides[currentSlide];
+        $('.section-main').css('background', slide.backgroundColor);
+
+        $('.screenshot').fadeOut(animationDelay, function () {
+            $('.screenshot').css('background', 'url(' + imagePath + slide.screenshot + ') top left / cover no-repeat');
+        }).fadeIn(animationDelay);
+        $('body').animate({backgroundColor: slide.bodyAnimationBackgroundColor}, 'fast');
+        $('.overlay').fadeTo('slow', 0.0, function () {
+            $('.overlay').css('background', 'url(' + imagePath + slide.overlayImage + ') 0 50% / cover no-repeat');
+        }).fadeTo('slow', 0.06);
+        currentSlide++;
+    }
+
+    setInterval(advanceSlide, slideAdvanceDelay);
+});
